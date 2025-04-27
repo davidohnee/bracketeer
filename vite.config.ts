112e4004a8abf1e6419b1104d1e58f45311229d8
dxstiny <dxstiny.gh@gmail.com>
2025-04-27 17:00:42 +0200

    move base to ENV

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: "/bracketeer/",
+    base: process.env.BASE,
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),

d289b5d011e6aa7b47e2585ec6778cbce0712aac
dxstiny <dxstiny.gh@gmail.com>
2025-05-15 17:15:13 +0200

    remove unused import

--- src/components/modals/ShareFullModal.vue
+++ src/components/modals/ShareFullModal.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from "vue";
+import { onMounted, ref } from "vue";
 import gistClient from "@/gistClient";
 import type { Tournament } from "@/types/tournament";
 import { useTournamentsStore } from "@/stores/tournaments";

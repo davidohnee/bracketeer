f0d0da0bc73167bed8b225408f8a80f0a6e643fc
davidohnee <dxstiny.gh@gmail.com>
2026-02-05 17:59:28 +0100

    vitest support node 25

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig, mergeConfig } from "vitest/config";
 import viteConfig from "./vite.config";
+import path from "node:path";
+import os from "node:os";
 
 export default mergeConfig(
     viteConfig,
     defineConfig({
         test: {
             environment: "happy-dom",
+            execArgv: [
+                "--localstorage-file",
+                path.resolve(os.tmpdir(), `vitest-${process.pid}.localstorage`),
+            ],
             coverage: {
                 provider: "v8", // or 'istanbul'
                 reporter: ["text", "lcov"],

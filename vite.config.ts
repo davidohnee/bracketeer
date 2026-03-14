ad7a22cac30a868b66226ebad76726919fabc1d8
davidohnee <dxstiny.gh@gmail.com>
2026-03-14 16:42:37 +0100

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vitest/config";
 import { fileURLToPath, URL } from "node:url";
 import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
+        vueDevTools(),
         vue({
             template: {
                 compilerOptions: {

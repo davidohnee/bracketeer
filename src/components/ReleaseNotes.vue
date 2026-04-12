0201a211ad5a156babdee5e3e024b299b93ccd46
davidohnee <dxstiny.gh@gmail.com>
2026-04-12 15:56:53 +0200

    release notes: add fallback message

--- src/components/ReleaseNotes.vue
+++ src/components/ReleaseNotes.vue
@@ -2,19 +2,31 @@
 import { onMounted, ref } from "vue";
 import MarkdownRender from "./MarkdownRender.vue";
 
-const repository = "davidohnee/bracketeer";
+const REPOSITORY = "davidohnee/bracketeer";
+const FALLBACK = `## bracketeer v${APP_VERSION}\n\nRelease notes are currently unavailable. Please refer to https://github.com/${REPOSITORY}/releases/tag/v${APP_VERSION} instead`;
 
 const releaseNotes = ref("");
 
 const loadReleaseNotes = async () => {
-    const url = `https://api.github.com/repos/${repository}/releases/tags/v${APP_VERSION}`;
+    const url = `https://api.github.com/repos/${REPOSITORY}/releases/tags/v${APP_VERSION}`;
     const res = await fetch(url);
+
+    if (!res.ok) {
+        releaseNotes.value = FALLBACK;
+        return;
+    }
+
     const release = await res.json();
+
+    if (!release.body) {
+        releaseNotes.value = FALLBACK;
+        return;
+    }
 
     const regex = /#(\d+)/gm;
     releaseNotes.value = release.body.replaceAll(
         regex,
-        `[#$1](https://github.com/${repository}/issues/$1)`,
+        `[#$1](https://github.com/${REPOSITORY}/issues/$1)`,
     );
 };
 

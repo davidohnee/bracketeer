d243129668f345bf5872ca2e23b656c8638b2a6f
davidohnee <dxstiny.gh@gmail.com>
2026-06-18 12:55:28 +0200

    share, gists: fix unintended recursion

--- src/helpers/share/gist/pushSync.ts
+++ src/helpers/share/gist/pushSync.ts
@@ -4,17 +4,31 @@ import { ref, watch, type Ref } from "vue";
 import GistClient from "@/helpers/share/gist";
 import { useAccountsStore } from "@/stores/accounts";
 import type { IPushSync } from "../pushSync";
+import diff from "microdiff";
 
 export const createPushSync = (tournament: Ref<Tournament | null>): IPushSync => {
     const accounts = useAccountsStore();
     let remoteIdentifier = "";
     let stopWatching: (() => void) | null = null;
+    let currentTournament: Tournament | null = null;
 
     const startWatching = () => {
         stopWatching?.();
         stopWatching = watch(
             tournament,
             () => {
+                const d = diff(currentTournament ?? {}, tournament.value ?? {});
+                if (
+                    !tournament.value ||
+                    !d.length ||
+                    d.every(
+                        (change) =>
+                            change.path.includes("remote") && change.path.includes("pushDate"),
+                    )
+                ) {
+                    return;
+                }
+                currentTournament = { ...tournament.value };
                 publish();
             },
             { deep: true },
